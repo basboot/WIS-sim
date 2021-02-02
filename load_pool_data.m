cb83ea2b85e60487cf9f5ca70f70e26f28ebb790
Bas Boot <b.boot@student.tudelft.nl>
2021-02-02 17:37:31 +0100

    Added script to load all pool data for Gabriel

---
+++ load_pool_data.m
@@ -0,0 +1,12 @@
+%% Load data sets for idfentification
+pool1_set1 = wis_data("20210126_step_gate1_2_s25_no_intake.csv", wis);
+pool1_set2 = wis_data("20210126_step_gate1_2_s100_no_intake.csv", wis);
+pool1_set3 = wis_data("20210126_step_gate1_2_s255_no_intake.csv", wis);
+
+pool2_set1 = wis_data("20210126_step_gate2_3_s25_no_intake.csv", wis);
+pool2_set2 = wis_data("20210126_step_gate2_3_s100_no_intake.csv", wis);
+pool2_set3 = wis_data("20210126_step_gate2_3_s255_no_intake.csv", wis);
+
+pool3_set1 = wis_data("20210126_step_gate3_4_s25_no_intake.csv", wis);
+pool3_set2 = wis_data("20210126_step_gate3_4_s100_no_intake.csv", wis);
+pool3_set3 = wis_data("20210126_step_gate3_4_s255_no_intake.csv", wis);
