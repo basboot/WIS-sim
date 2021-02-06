1d65ff37db10de23c274c548d504bf763ddeeb4f
Bas Boot <b.boot@student.tudelft.nl>
2021-02-06 16:30:22 +0100

    Renamed water height to water level

--- time_plot.m
+++ time_plot.m
@@ -1,10 +1,10 @@
 %% show graph to check calibration
 
 plot_data = ...
-    wis_data("20210202_step_gate3_4_s50_no_intake3.csv", wis);
+    wis_data("20210202_step_gate2_3_s255_no_intake.csv", wis);
 
 figure(3);
-title("Water heights");
-plot(plot_data.timing/1000, plot_data.water_heights);
+title("Water levels");
+plot(plot_data.timing/1000, plot_data.water_levels);
 legend("s1", "s2", "s3", "s4", "s5", "s6", "s7")
                   
