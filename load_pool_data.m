00e3c0f6c7b475328ef89a4e9a5e5c26c914fcba
Bas Boot <b.boot@student.tudelft.nl>
2021-02-06 16:30:33 +0100

    Added new data sets

--- load_pool_data.m
+++ load_pool_data.m
@@ -1,12 +1,35 @@
 %% Load data sets for idfentification
-pool1_set1 = wis_data("20210126_step_gate1_2_s25_no_intake.csv", wis);
-pool1_set2 = wis_data("20210126_step_gate1_2_s100_no_intake.csv", wis);
-pool1_set3 = wis_data("20210126_step_gate1_2_s255_no_intake.csv", wis);
+% pool1_set1 = wis_data("20210126_step_gate1_2_s25_no_intake.csv", wis);
+% pool1_set2 = wis_data("20210126_step_gate1_2_s100_no_intake.csv", wis);
+% pool1_set3 = wis_data("20210126_step_gate1_2_s255_no_intake.csv", wis);
+% 
+% pool2_set1 = wis_data("20210126_step_gate2_3_s25_no_intake.csv", wis);
+% pool2_set2 = wis_data("20210126_step_gate2_3_s100_no_intake.csv", wis);
+% pool2_set3 = wis_data("20210126_step_gate2_3_s255_no_intake.csv", wis);
+% 
+% pool3_set1 = wis_data("20210126_step_gate3_4_s25_no_intake.csv", wis);
+% pool3_set2 = wis_data("20210126_step_gate3_4_s100_no_intake.csv", wis);
+% pool3_set3 = wis_data("20210126_step_gate3_4_s255_no_intake.csv", wis);
 
-pool2_set1 = wis_data("20210126_step_gate2_3_s25_no_intake.csv", wis);
-pool2_set2 = wis_data("20210126_step_gate2_3_s100_no_intake.csv", wis);
-pool2_set3 = wis_data("20210126_step_gate2_3_s255_no_intake.csv", wis);
 
-pool3_set1 = wis_data("20210126_step_gate3_4_s25_no_intake.csv", wis);
-pool3_set2 = wis_data("20210126_step_gate3_4_s100_no_intake.csv", wis);
-pool3_set3 = wis_data("20210126_step_gate3_4_s255_no_intake.csv", wis);
+%% Load data sets for idfentification
+pool1_set1 = wis_data("20210202_step_gate1_2_s25_no_intake.csv", wis);
+pool1_set2 = wis_data("20210202_step_gate1_2_s100_no_intake.csv", wis);
+pool1_set3 = wis_data("20210202_step_gate1_2_s255_no_intake.csv", wis);
+
+pool2_set1 = wis_data("20210202_step_gate2_3_s25_no_intake.csv", wis);
+pool2_set2 = wis_data("20210202_step_gate2_3_s100_no_intake.csv", wis);
+pool2_set3 = wis_data("20210202_step_gate2_3_s255_no_intake.csv", wis);
+
+pool3_set1 = wis_data("20210202_step_gate3_4_s25_no_intake.csv", wis);
+pool3_set2 = wis_data("20210202_step_gate3_4_s100_no_intake.csv", wis);
+pool3_set3 = wis_data("20210202_step_gate3_4_s255_no_intake.csv", wis);
+pool3_set4 = wis_data("20210202_step_gate3_4_s50_no_intake.csv", wis);
+
+pool3_set1b = wis_data("20210202_step_gate3_4_s25_no_intake2.csv", wis);
+pool3_set2b = wis_data("20210202_step_gate3_4_s100_no_intake2.csv", wis);
+pool3_set3b = wis_data("20210202_step_gate3_4_s255_no_intake2.csv", wis);
+pool3_set4b = wis_data("20210202_step_gate3_4_s50_no_intake2.csv", wis);
+
+pool3_set4c = wis_data("20210202_step_gate3_4_s50_no_intake3.csv", wis);
+
