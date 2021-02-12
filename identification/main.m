6e7f3dbb184fbbd8792a43f68dbf3e72e4b59917
Bas Boot <b.boot@student.tudelft.nl>
2021-02-12 11:52:48 +0100

    Update main.m
    Save results for use in simulation

--- identification/main.m
+++ identification/main.m
@@ -1,23 +1,26 @@
 clear all;
 
-%
+%% Add common functions to path
 addpath ../functions/
 
-% set global properties
+%% set global properties
 wis_properties;
 
-% perform calibration
+%% perform calibration
 calibration;
 
-% visual validation of calibration
+%% visual validation of calibration
 time_plot;
 
-% load data sets
+%% load data sets
 load_pool_data;
 
-% identify flow over the gates
+%% identify flow over the gates
 gate_identification;
 
-% identify transfer function for the pools
+%% identify transfer function for the pools
 identification;
+
+%% save results for later use
+save("identification.mat", 'wis', 'pool_model')
 
