c9a37c3e08e6dbc57f933cbc41a16f191e74b923
Bas Boot <b.boot@student.tudelft.nl>
2021-02-21 17:08:10 +0100

    Created script to write C code for the sensor calibration settings

---
+++ identification/sensor_parameters_c.m
@@ -0,0 +1,13 @@
+fprintf("// START - Calibration values calculated in MATLAB\n")
+
+fprintf("static uint16_t sensor1_offsets[NUM_PRIMARY_SENSORS] = {%d, %d, %d, %d};\n", ...
+    Wis.b(1), Wis.b(3), Wis.b(5), Wis.b(7));
+fprintf("static uint16_t sensor2_offsets[NUM_SECONDARY_SENSORS] = {%d, %d, %d};\n", ...
+    Wis.b(2), Wis.b(4), Wis.b(6));
+
+fprintf("static uint16_t sensor1_scalings[NUM_PRIMARY_SENSORS] = {%d, %d, %d, %d};\n", ...
+    Wis.a(1), Wis.a(3), Wis.a(5), Wis.a(7));
+fprintf("static uint16_t sensor2_scalings[NUM_SECONDARY_SENSORS] = {%d, %d, %d};\n", ...
+    Wis.a(2), Wis.a(4), Wis.a(6));
+
+fprintf("// END - Calibration values calculated in MATLAB\n")
