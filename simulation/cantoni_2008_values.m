886f3d98cd3f9ecf490d703a2599ff46d3f36117
Bas Boot <b.boot@student.tudelft.nl>
2021-02-26 16:54:32 +0100

    Added pool 6 for experimenting

--- simulation/cantoni_2008_values.m
+++ simulation/cantoni_2008_values.m
@@ -1,12 +1,12 @@
 %% set values for cantoni_LMI (Cantoni 2008)
 
-nPool = 5;
+nPool = 5; % added 6th pool to test nPool script
 
 % TODO: remove: data present in PoolModel (in secs and rad/sec!!!)
 % % Pool model parameters
-tau = [4, 2, 4, 4, 6]; % minutes
-alpha = [6492, 2478, 6084, 5658, 7650]; % m^2
-phi_wave = [0.48, 1.05, 0.48, 0.48, 0.42 ]; % rad/min (wave frequency)
+tau = [4, 2, 4, 4, 6, 4]; % minutes
+alpha = [6492, 2478, 6084, 5658, 7650, 6492]; % m^2
+phi_wave = [0.48, 1.05, 0.48, 0.48, 0.42 , 0.48]; % rad/min (wave frequency)
 
 % CVX suddenly decided to not be able to find its own function 'vec', so:
 %addpath C:\Users\Jacob\Documents\MATLAB\CVX\cvx\functions\vec_
@@ -37,10 +37,10 @@ end
 
 % TODO: tune parameters / use code from simulation_lab
 % Loop shaping weights parameters from [1]
-kappa = [1.69, 6.47, 2.37, 2.21, 1.68];
-phi = [113.64, 37.17, 86.96, 96.15, 113.64];
-rho = [9.97, 3.26, 7.60, 8.47, 9.97];
-% TODO: BB: eta seems unused, find out what it is for
-eta = [130, 223, 183, 170, 153]; 
+kappa = [1.69, 6.47, 2.37, 2.21, 1.68, 1.69];
+phi = [113.64, 37.17, 86.96, 96.15, 113.64, 113.64];
+rho = [9.97, 3.26, 7.60, 8.47, 9.97, 9.97];
+% BBB: eta is only used for scaling of the plots
+eta = [130, 223, 183, 170, 153, 130]; 
 
 % %% Define the matrices
