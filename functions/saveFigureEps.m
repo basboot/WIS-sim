4432991c18349dd0cb88e9fe55bb068756729b44
Bas Boot <b.boot@student.tudelft.nl>
2021-07-27 15:52:08 +0200

    Increased font size of plots

--- functions/saveFigureEps.m
+++ functions/saveFigureEps.m
@@ -1,9 +1,16 @@
-function [] = saveFigureEps(figureName)
+function [] = saveFigureEps(figureName, fontSize)
 %SAVEFIGURE Saves the current figure
 
 % By putting this in a separate function it is easy to change the location
 % or disable saving for all figures
 
+    hold on;
+    if nargin > 1
+        set(findobj(gcf,'type','axes'),'FontSize', fontSize);
+    else
+        set(findobj(gcf,'type','axes'),'FontSize', 18); % default fontSize
+    end
+    
     imageLocation = '/Users/bas/Dropbox/Apps/Overleaf/Master Thesis/images';
     
     %saveas(gcf,sprintf('%s/%s', imageLocation, figureName), 'epsc');
