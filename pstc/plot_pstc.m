e621ea0aeb46ebface6c9c565f6be1fa88d62509
Bas Boot <b.boot@student.tudelft.nl>
2021-08-11 15:59:08 +0200

    Added script for plotting

--- pstc/plot_pstc.m
+++ pstc/plot_pstc.m
@@ -9,7 +9,24 @@ showPlots = true;
 useCachedData = true;
 
 nExperiments = 1;
-experiments{1} = ["20210806pstc_simulation", "PSTC", "(test)", "20210806pstc_controller"];
+experiments{1} = ["20210810test0-01_1_force_trigger_python", "PSTC", "(force trigger)", "20210810test0-01_1_force_trigger_controller"];
+
+%experiments{1} = ["20210810test0-01_1_no_trigger_python", "PSTC", "(no trigger)", "20210810test0-01_1_no_trigger_controller"];
+
+%experiments{1} = ["20210810test0-01_1_python2", "PSTC", "(0.01 1)", "20210810test0-01_1_controller2"];
+
+%experiments{1} = ["20210810test0-01_1_1pct_controller", "PSTC", "(0.01 1)", "20210810test0-01_1_1pct_controller"];
+
+%experiments{1} = ["20210810test0-01_1_python_ss", "PSTC", "(0.01 1)", "20210810test0-01_1_controller_ss"];
+
+%experiments{1} = ["20210810test0-1_2_0-1pct_python", "PSTC", "(0.1 2)", "20210810test0-1_2_0-1pct_controller"];
+
+% possibly not the same run :-(
+%experiments{1} = ["20210811test0-1_1_python", "PSTC", "(0.1 1)", "20210811test0-1_1_controller"];
+
+experiments{1} = ["20210811test0-05_1_python", "PSTC", "(0.05 1)", "20210811test0-05_1_controller"];
+
+
 % filename sim, sigma, epsilon, filename contr
 
 for iExperiments = 1: nExperiments
@@ -19,19 +36,23 @@ for iExperiments = 1: nExperiments
     
     fileName1 = sprintf('%s.mat', experimentNameSim);
     fileName2 = sprintf('%s.mat', experimentNameCtrl);
+    fileName3 = sprintf('%s.mat', "sim_0-01-1");
+    %fileName3 = sprintf('%s.mat', "sim_forcetrigger");
 
     
     %% Load cached data
     try
-        load(fileName1);
+        sensors = load(fileName1);
+        controller = load(fileName2);
+        sim = load(fileName3);
     catch
-        assert(false, "Data file sim does not exist");
+        assert(false, "Data file does not exist");
     end
 
 
     if showPlots   
         figure();
-        plot(y_log');
+        plot(sensors.y_log');
         hold on;
 
         xlabel('time (s)')
@@ -52,13 +73,13 @@ for iExperiments = 1: nExperiments
     if showPlots
         figure();
         yyaxis left
-        plot(radio_log(1:3,:)');
+        plot(sensors.radio_log');
         ylabel('radio on (ms)')
         ylim([0 100]) % fix scale for comparison and crop to avoid scaling caused by single outliers
 
         hold on;
         yyaxis right
-        plot(cumsum(radio_log')/1000);
+        plot(cumsum(sensors.radio_log')/1000);
 
         xlabel('time (s)')
         ylabel('total radio on (s)')
@@ -72,15 +93,15 @@ for iExperiments = 1: nExperiments
 
 
     % take average of 3 FF for more accurate result
-    total_on = (sum(sum(radio_log'))/3)/1000; %s
+    total_on = (sum(sum(sensors.radio_log'))/3)/1000; %s
 
-    samples = size(y_log, 2);
+    samples = size(sensors.y_log, 2);
 
-    triggers = sum(sum(((radio_log')) > 30))/3;
+    triggers = sum(sum(((sensors.radio_log')) > 30))/3;
 
     trigger_ratio = triggers / samples;
 
-    error = (y_log' - target) * 1000; % mm
+    error = (sensors.y_log' - target) * 1000; % mm
     mse = sum(sum((error .^2))) / (size(error, 1) );
 
     ise = 0;
@@ -101,39 +122,65 @@ for iExperiments = 1: nExperiments
     %disp(experimentName);
     disp(sprintf("%s & %s & %.1f & %.2f & %.0f & %.0f  \\\\", experiments{iExperiments}(2), experiments{iExperiments}(3), total_on, trigger_ratio, mse, mtse));
 
-        %% Load cached data
-    try
-        load(fileName2);
-    catch
-        assert(false, "Data file pstc controller does not exist");
-    end
+
+    figure()
+    plot(sensors.u_log');
+    title('Sim - Control signal');
+    
+    % PSTC plots
+    figure()
+    plot(controller.u_log');
+    ylim([0 20]);
+    title('PSTC - Control signal');
+    
+    figure()
+    plot(controller.dk_log');
+    title('PSTC - Calculated sleeping times');
+    
+    figure()
+    plot(controller.t_log');
+    title('PSTC - Triggers');
+    
+    figure()
+    plot(controller.initialized_log');
+    title('PSTC - Initialisation state');
+    
+    figure()
+    plot(controller.y_log');
+    title('PSTC - Water levels');
+    
+    figure()
+    plot(controller.radio_log');
+    title('PSTC - Radio');
+    
+    
+%     %% simulation for comparison
+%     if showPlots   
+%         figure();
+%         plot(sim.y_log'/1000 + [0.25 0.20 0.15]);
+%         hold on;
+% 
+%         xlabel('time (s)')
+%         ylabel('level (m)')
+%         legend('pool1', 'pool2', 'pool3');
+% 
+%         yline(0.25,'-','reference 1', 'LabelHorizontalAlignment', 'left', 'HandleVisibility','off');
+%         yline(0.20,'-','reference 2', 'LabelHorizontalAlignment', 'left', 'HandleVisibility','off');
+%         yline(0.15,'-','reference 3', 'LabelHorizontalAlignment', 'left', 'HandleVisibility','off');
+% 
+%         saveFigureEps(sprintf("%s-levels", experimentNameSim));
+% 
+%         title("FULL Simulation Water levels");
+%         
+%         figure()
+%         plot(sim.u_log');
+%         ylim([0 20]);
+%         title('FULL SIM - Control signal');
+%     end
+    
+  
+    
 end
 
-% % .02 9.1252e-04 (37.47)
-% % 0.01 5.0639e-04 (38.31s)
-% % 0.00125 4.0085e-04 (46.43)
-% % per 3.9312e-04
-% 
-% %% Check 0,0 setting
-% % convert levels to pressure value
-% s1 = m2pressure(level.Data(:,1), Wis.a(3), Wis.b(3));
-% s2 = m2pressure(level.Data(:,2), Wis.a(5), Wis.b(5));
-% s3 = m2pressure(level.Data(:,3), Wis.a(7), Wis.b(7));
-% 
-% % combine sensor data, and keep only 1 sample per second
-% sensor_data = [s1(1:SPS:end) s2(1:SPS:end) s3(1:SPS:end)];
-% 
-% % find where nothing changes changes
-% sameValues = 0;
-% for i = 2:samples
-%     if (sensor_data(i-1, 1) == sensor_data(i, 1)) && (sensor_data(i-1, 2) == sensor_data(i, 2)) && (sensor_data(i-1, 3) == sensor_data(i, 3))
-%         sameValues = sameValues + 1;
-%         level.Data(i-1,1);
-%         level.Data(i,1);
-%         %disp('---');
-%     end
-% end
-% 
-% disp(sprintf("Same values at %d epochs, so with sigma = epsilon = 0 this would result in a trigger-ratio of: %f", sameValues, (samples - sameValues)/samples));
 
-% minimum 
+
