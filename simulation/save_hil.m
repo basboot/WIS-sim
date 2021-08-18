22147d06ec9d3460e0a0622d13985009ac894c72
Bas Boot <b.boot@student.tudelft.nl>
2021-08-18 20:49:14 +0200

    More saves

--- simulation/save_hil.m
+++ simulation/save_hil.m
@@ -30,7 +30,39 @@
 %experimentName = "continuous_disturbance_etc_0-1-1";
 
 % NEW: Pade for PSTC
-experimentName = "continuous_disturbance_etc_0-0025-0-01-pade";
+%experimentName = "continuous_disturbance_etc_0-0025-0-01-pade";
+
+%experimentName = "20210805test_pstc_force_trigger";
+
+% NEW FIXED TRIGGERING
+
+%experimentName = "new_triggering_etc_0-1_1";
+
+%experimentName = "new_triggering_etc_0-05_1";
+
+%experimentName = "new_triggering_etc_0-025_1";
+
+%experimentName = "new_triggering_etc_0-05_2";
+
+%experimentName = "new_triggering_etc_0-025_2";
+
+%experimentName = "new_triggering_etc_0-1_2";
+
+%experimentName = "new_triggering_etc_0-2_2";
+
+%experimentName = "new_triggering_etc_0-4_2";
+
+%experimentName = "new_triggering_etc_0-4_4-bad";
+
+%experimentName = "new_triggering_etc_0-4_4";
+
+%experimentName = "new_triggering_etc_0-4_8";
+
+%experimentName = "new_triggering_etc_0-4_16";
+
+%experimentName = "new_triggering_etc_0-2_16";
+
+%--
 
 fileName = sprintf('mat/hil_%s.mat', experimentName);
 
